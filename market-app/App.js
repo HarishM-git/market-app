adda6adc811b3dc8ba867a71394f9233cb6cb7fe
harishm-git <harishdeepakh@gmail.com>
2024-10-16 19:37:39 +0530

    starting a new pro

---
+++ market-app/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up the app.js file to work with me instead of seeing the one piece ok you got it right </Text>
+      <StatusBar style="auto" />
+    </View> 
+  ); 
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
