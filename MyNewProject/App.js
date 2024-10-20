cd9cac53ebc33adc31610fe29912e15277889f9c
harishm-git <harishdeepakh@gmail.com>
2024-10-20 23:07:42 +0530

    working on the new pro

--- MyNewProject/App.js
+++ MyNewProject/App.js
@@ -4,10 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up the app.js file to work with me instead of seeing the one piece ok you got it right </Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View> 
-  ); 
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
